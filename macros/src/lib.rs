12e3aea36454157872569e4342a1bd4eb650f927
Pained Pineapple <github.com.nineteen128@passmail.net>
2024-05-30 13:41:48 -0700

    disable toolbar, fix render and reload mount bug

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -9,80 +9,80 @@ pub fn preview(_attrs: TokenStream, input: TokenStream) -> TokenStream {
     let vis = item.vis.clone();
     let block = item.block.clone();
 
-    let docs = collect_docs(&item.attrs);
+    // let docs = collect_docs(&item.attrs);
 
     let s = ident.to_string();
     let name = s.strip_suffix("Preview").unwrap_or(&s);
 
-    let mut states = Vec::new();
-    let mut from_states = Vec::new();
+    // let mut states = Vec::new();
+    // let mut from_states = Vec::new();
     // let mut controls = Vec::new();
 
-    for arg in item.sig.inputs.into_iter().skip(1) {
-        match arg {
-            FnArg::Typed(typed_arg) => {
-                let mut docs = String::new();
-                let mut default: Option<Expr> = None;
+    // for arg in item.sig.inputs.into_iter().skip(1) {
+    //     match arg {
+    //         FnArg::Typed(typed_arg) => {
+    // let mut docs = String::new();
+    // let mut default: Option<Expr> = None;
 
-                for attr in typed_arg.attrs {
-                    let path = attr.path().get_ident().unwrap().to_string();
-                    if path == "doc" {
-                        let meta = attr.meta.require_name_value().unwrap();
-                        if let Expr::Lit(expr) = &meta.value {
-                            if let Lit::Str(lit) = &expr.lit {
-                                docs.push_str(&lit.value());
-                                docs.push('\n');
-                            }
-                        }
-                    }
-                    // else if path == "lookbook" {
-                    //     let path = attr.meta.require_list().unwrap();
-                    //     let meta: Meta = syn::parse2(path.tokens.clone()).unwrap();
+    // for attr in typed_arg.attrs {
+    //     let path = attr.path().get_ident().unwrap().to_string();
+    // if path == "doc" {
+    //     let meta = attr.meta.require_name_value().unwrap();
+    //     if let Expr::Lit(expr) = &meta.value {
+    //         if let Lit::Str(lit) = &expr.lit {
+    //             docs.push_str(&lit.value());
+    //             docs.push('\n');
+    //         }
+    //     }
+    // }
+    // else if path == "lookbook" {
+    //     let path = attr.meta.require_list().unwrap();
+    //     let meta: Meta = syn::parse2(path.tokens.clone()).unwrap();
 
-                    //     if let Meta::NameValue(meta_name_value) = meta {
-                    //         if meta_name_value.path.is_ident("default") {
-                    //             let value = meta_name_value.value;
-                    //             default = Some(value);
-                    //         }
-                    //     }
-                    // }
-                }
+    //     if let Meta::NameValue(meta_name_value) = meta {
+    //         if meta_name_value.path.is_ident("default") {
+    //             let value = meta_name_value.value;
+    //             default = Some(value);
+    //         }
+    //     }
+    // }
+    // }
 
-                let ty = typed_arg.ty;
-                let pat = typed_arg.pat.clone();
-                let pat_name = pat.to_token_stream().to_string();
+    // let ty = typed_arg.ty;
+    // let pat = typed_arg.pat.clone();
+    // let pat_name = pat.to_token_stream().to_string();
 
-                states.push(quote! {
-                    let default = #default;
-                    let #pat = use_signal(|| default);
-                });
+    // states.push(quote! {
+    //     let default = #default;
+    //     let #pat = use_signal(|| default);
+    // });
 
-                // from_states.push(quote! {let #pat = <#ty>::from_state(&**#pat);});
+    // from_states.push(quote! {let #pat = <#ty>::from_state(&**#pat);});
 
-                // from_states.push(quote! {let #pat = <#ty>::from_state(#pat);});
-                from_states.push(quote! {
-                    // tracing::info!("hello world 4, {:?}, {:?}", stringify!(#pat), stringify!(#ty));
-                });
+    // from_states.push(quote! {let #pat = <#ty>::from_state(#pat);});
+    // from_states.push(quote! {
+    // tracing::info!("hello world 4, {:?}, {:?}", stringify!(#pat), stringify!(#ty));
+    // });
 
-                // let ty_name = ty.span().source_text().unwrap_or_else(|| "".to_string());
-                // let default_string = default
-                //     .map(|expr| expr.span().source_text().unwrap_or_default())
-                //     .unwrap_or_default();
+    // let ty_name = ty.span().source_text().unwrap_or_else(|| "".to_string());
+    // let default_string = default
+    //     .map(|expr| expr.span().source_text().unwrap_or_default())
+    //     .unwrap_or_default();
 
-                // controls.push(quote! {{ rsx! {
-                //     tr {
-                //         border_bottom: "2px solid #e7e7e7",
-                //         td { padding_left: "20px", p { color: "#222", font_weight: 600, #pat_name } }
-                //         td { code { #ty_name } }
-                //         td { p { #docs } }
-                //         td { code { #default_string } }
-                //         // td { padding_right: "20px", <#ty>::control(#pat_name, #pat) }
-                //     }
-                // }}});
-            }
-            _ => todo!(),
-        }
-    }
+    // controls.push(quote! {{ rsx! {
+    //     tr {
+    //         border_bottom: "2px solid #e7e7e7",
+    //         td { padding_left: "20px", p { color: "#222", font_weight: 600, #pat_name } }
+    //         td { code { #ty_name } }
+    //         td { p { #docs } }
+    //         td { code { #default_string } }
+    //         // td { padding_right: "20px", <#ty>::control(#pat_name, #pat) }
+    //     }
+    // }}});
+    // }
+    //         _ => todo!(),
+    //     }
+    // }
 
     // let controls = render_with_location(
     //     quote! {
@@ -96,7 +96,8 @@ pub fn preview(_attrs: TokenStream, input: TokenStream) -> TokenStream {
         quote! {
             lookbook::Look {
                 name: #name,
-                docs: #docs,
+                // docs: #docs,
+                docs: "",
                 // controls: controls,
                 controls: None,
                 children: #block
@@ -122,13 +123,13 @@ pub fn preview(_attrs: TokenStream, input: TokenStream) -> TokenStream {
             // tracing::info!("hello world 4, {:?}", stringify!(#controls));
 
             fn f() -> Element {
-                #(#states)*
+                // #(#states)*
 
                 // let controls = rsx! { #controls };
 
                 // #(#debug_stuff)*
 
-                #(#from_states)*
+                // #(#from_states)*
                 // #debug_stuff
 
                 // rsx! { div { "Hello, world!" } }
--- src/ui/pane.rs
+++ src/ui/pane.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
-use dioxus_resize_observer::use_size;
-use dioxus_use_mounted::use_mounted;
+// use dioxus_resize_observer::use_size;
+// use dioxus_use_mounted::use_mounted;
 
 #[component]
 pub fn HorizontalPane(left: Element, right: Element) -> Element {
@@ -38,22 +38,22 @@ pub fn HorizontalPane(left: Element, right: Element) -> Element {
 
 #[component]
 pub fn VerticalPane(top: Element, bottom: Element) -> Element {
-    let container_ref = use_mounted();
-    let container_size = use_size(container_ref);
+    // let container_ref = use_mounted();
+    // let container_size = use_size(container_ref);
 
-    let mut height = use_signal(|| 0.);
+    // let mut height = use_signal(|| 0.);
 
-    let mut count = use_signal(|| 0);
+    // let mut count = use_signal(|| 0);
 
-    use_future(move || {
-        if *count.peek() <= 2 {
-            height.set(height / 2.);
-            count += 1;
-        }
-        async {}
-    });
+    // use_future(move || {
+    //     if *count.peek() <= 2 {
+    //         height.set(height / 2.);
+    //         count += 1;
+    //     }
+    //     async {}
+    // });
 
-    let mut is_dragging = use_signal(|| false);
+    // let mut is_dragging = use_signal(|| false);
 
     // is_dragging.peek()
     rsx! {
@@ -62,15 +62,15 @@ pub fn VerticalPane(top: Element, bottom: Element) -> Element {
             flex: 1,
             display: "flex",
             flex_direction: "column",
-            onmounted: move |event| container_ref.onmounted(event),
+            // onmounted: move |event| container_ref.onmounted(event),
             div {
                 position: "absolute",
-                display: if *is_dragging.peek() { "block" } else { "none" },
+                // display: if *is_dragging.peek() { "block" } else { "none" },
                 width: "100%",
                 height: "100%",
-                onmouseup: move |_| { is_dragging.set(false) },
-                prevent_default: if *is_dragging.peek() { "onmousedown onmousemove" } else { "" },
-                onmousemove: move |event| height.set(container_size.height() - event.data.client_coordinates().y)
+                // onmouseup: move |_| { is_dragging.set(false) },
+                // prevent_default: if *is_dragging.peek() { "onmousedown onmousemove" } else { "" },
+                // onmousemove: move |event| height.set(container_size.height() - event.data.client_coordinates().y)
             }
             {top},
             div {
@@ -78,10 +78,10 @@ pub fn VerticalPane(top: Element, bottom: Element) -> Element {
                 padding: "5px 0",
                 margin: "-5px 0",
                 cursor: "ns-resize",
-                onmousedown: move |_| { is_dragging.set(true) },
+                // onmousedown: move |_| { is_dragging.set(true) },
                 div { height: "2px", width: "100%", background: "#ccc" }
             }
-            div { display: "flex", flex_direction: "column", height: "{height}px", overflow: "auto", {bottom} }
+            // div { display: "flex", flex_direction: "column", height: "auto", overflow: "auto", {bottom} }
         }
     }
 }
